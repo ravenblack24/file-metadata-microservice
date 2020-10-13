8ed9a0c3ffa648cfdf4096ddc8e110e11d805890
Kay Pea <19981420+ravenblack24@users.noreply.github.com>
2020-10-13 14:35:14 +0100

    documentation

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Init project
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
@@ -5,23 +6,36 @@ const {getFileMetadata} = require('./src');
 
 const app = express();
 
+// Serve static resources from public dir
 app.use(express.static(__dirname+"/public"));
 app.use(cors());
+
+// Parse URL-encoded bodies
 app.use(express.urlencoded({extended:false}));
+// Enable file upload
 app.use(fileUpload({
     createParentPath: true
 }));
 
+// Route for index page
 app.get("/", (req, res) => {
     res.sendFile(__dirname+"/views/index.html");
 });
 
+/**
+ * Process file upload and return Object
+ * 
+ * @param {Request} req the request containing a file upload
+ * 
+ * @returns {Object} Object containing error key value pair or file metadata
+ */
 app.post("/api/fileanalyse", (req, res) => {
     getFileMetadata(req, res);
 });
 
 const port = process.env.PORT || 3000;
 
+// Listen for requests
 var listener = app.listen(port, () => {
     console.log("Your app is listening on port " + listener.address().port);
 });
--- src/getFileMetadata.js
+++ src/getFileMetadata.js
@@ -1,3 +1,13 @@
+
+/**
+ * Process file uploaded in the POST request
+ * 
+ * @param {Request}  req  the file uploaded in the request
+ * @param {Response} res  the response to the request
+ * 
+ * @returns {String} 
+ * 
+ */
 const getFileMetadata = (req, res) => {
     if(!req.files || Object.keys(req.files).length === 0) {
         return res.json({"error": "No file uploaded"});
